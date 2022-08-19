fe5e83332bb0dc0cbbfe59310f4df0ec6791a064
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-08-19 14:12:45 +0100

    navbar corrected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './NavBar.css';
+import photo from "./logo.svg";
 
 function NavBar() {
     return (
         <nav>
-            <img src="../images/react-icon-small.png" />
+            <img src={photo} />
             <h3 className="first">ReactFacts</h3>
             <h4 className="later">React Course - Project 1</h4>
         </nav>
